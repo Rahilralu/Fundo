4734435792730303d736e6aa23ad0bc56eda70c7
Rahilralu <nazarrahil0000@gmail.com>
2026-06-08 07:54:53 +0530

    logging

--- backend/src/services/mailer.service.js
+++ backend/src/services/mailer.service.js
@@ -4,16 +4,17 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export async function sendEmail({ to, subject, text, html }) {
   try {
-    await sgMail.send({
+    console.log('Attempting to send email to:', to);
+    const response = await sgMail.send({
       from: 'nazarrahil0000@gmail.com',
       to,
       subject,
       text,
       html,
     });
-    console.log('Email sent successfully');
+    console.log('Email sent successfully, status:', response[0].statusCode);
   } catch (err) {
-    console.error('Error while sending mail:', err.response?.body || err);
+    console.error('Error while sending mail:', JSON.stringify(err.response?.body || err));
     throw err;
   }
 }
