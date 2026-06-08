1625b75450d303584d60e8670ed49cc85e079d35
Rahilralu <nazarrahil0000@gmail.com>
2026-06-08 07:16:06 +0530

    deployment fixes

--- backend/src/services/mailer.service.js
+++ backend/src/services/mailer.service.js
@@ -4,15 +4,21 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail({ to, subject, text, html }) {
   try {
-    const info = await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: 'Fundo Team <onboarding@resend.dev>',
       to,
       subject,
       text,
       html,
     });
-    console.log("Message sent:", info.data?.id);
-    return info;
+    
+    if (error) {
+      console.error("Resend error:", error);
+      throw error;
+    }
+    
+    console.log("Message sent:", data?.id);
+    return data;
   } catch (err) {
     console.error("Error while sending mail:", err);
     throw err;
